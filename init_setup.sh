ded29b684c0f5b2a57250689ca13691d90ccf9bb
taeefnajib <taeefnajib@gmail.com>
2023-11-22 02:13:46 +0600

    version locking is removed from everywhere

--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.8"
+echo [$(date)]: "Creating conda env with python 3.10"
 conda create --prefix ./env python=3.10 -y
 echo [$(date)]: "activate env"
 source activate ./env
