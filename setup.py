d6f0f6d272c048af18287c65f02ff8da8659b4fc
taeefnajib <taeefnajib@gmail.com>
2023-11-22 01:00:18 +0600

    python package files are added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "ficto"
+AUTHOR_USER_NAME = "taeefnajib"
+SRC_REPO = "ficto"
+AUTHOR_EMAIL = "taeefnajib@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package to generate a realistic but fictitious data in csv or json format.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
