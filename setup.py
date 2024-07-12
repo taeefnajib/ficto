45f47dfbf1293613f887cadc2c42e397c86101e3
taeefnajib <taeefnajib@gmail.com>
2024-07-13 04:45:29 +0600

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 REPO_NAME = "ficto"
 AUTHOR_USER_NAME = "taeefnajib"
